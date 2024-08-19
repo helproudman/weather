8d8f77bf03dc524a6422b1fc5b976ac551685e8f
Helen Proudman <helen.proudman@students.codeinstitute.net>
2024-08-19 22:30:28 +0000

    temperatures rounded to integers, some code in place for multiple events on event listener, modal added for blank location

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,11 @@ const currentWeatherIcon = document.getElementById("current-weather-icon");
 const currentSummary = document.getElementById("current-summary");
 const forecast = document.getElementById("forecast");
 
-locationSearch.addEventListener("click", callAPI);
+const events = [click, mouseover, onkeydown];
+events.forEach(event => {
+    locationSearch.addEventListener(event, () => {
+
+// locationSearch.addEventListener("click", callAPI);
 
 //openweathermap.api used 
 let apiKey = '752dd876d2d8e2f3f4886c7c478bb5d5';
@@ -14,6 +18,13 @@ let apiKey = '752dd876d2d8e2f3f4886c7c478bb5d5';
 // function set up to call the api and add promises
 function callAPI() {
     let locationName = locationInput.value;
+    
+
+
+
+    if (locationName === "") {
+        alert ("Please add a location to search for");
+    }
 
     let apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${locationName}&appid=${apiKey}&units=metric`;
 
@@ -24,9 +35,9 @@ function callAPI() {
         .then(function (data) {
                 //set the data for current day
                 console.log(data);
-                let temperatureToday = data.list[0].main.temp;
+                let temperatureToday = Math.round(data.list[0].main.temp);
                 console.log(temperatureToday);
-                document.getElementById("current-temperature").innerHTML = `Today's temperature: ${temperatureToday}`;
+                document.getElementById("current-temperature").innerHTML = `Today's temperature: ${temperatureToday}°C`;
 
                 // let iconToday = data.list[0].weather[0].icon;
                 // console.log(iconToday);
@@ -48,12 +59,12 @@ function callAPI() {
         )
     // function to call data for each day and add to html
     function addForecast(data, day) {
-        let temperature = `${data.main.temp}°C`;
+        let temperature = `${Math.round(data.main.temp)}°C`;
         // console.log(temperature);
         let summary = data.weather[0].description;
         let windSpeed = data.wind.speed;
 
-        let forecastString = `<div class="col-sm-12, col-md-4 col-lg-3"> ${temperature} ${summary}${windSpeed} </div>`;
+        let forecastString = `<div class="col-sm-12, col-md-4 col-lg-3"> ${temperature} <br>${summary}<br>${windSpeed} </div>`;
         document.getElementById("forecast").innerHTML += forecastString;
 
     }
